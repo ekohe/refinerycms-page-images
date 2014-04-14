4a07e96236c2cc5fd439283da6eeef692b30aab9
Maxime Guilbot <maxime@ekohe.com>
2014-04-14 12:12:20 +0800

    Fixed image_attributes= so that it doesn't affect the database in order for preview to work. image_attributes= will just save in memory and #save will persist to the db

--- lib/refinery/page_images/extension.rb
+++ lib/refinery/page_images/extension.rb
@@ -2,29 +2,38 @@ module Refinery
   module PageImages
     module Extension
       def has_many_page_images
-        has_many :image_pages, proc { order('position ASC') }, :as => :page, :class_name => 'Refinery::ImagePage'
-        has_many :images, proc { order('position ASC') }, :through => :image_pages, :class_name => 'Refinery::Image'
+        has_many :image_pages, :as => :page, :class_name => 'Refinery::ImagePage', :order => 'position ASC'
+        has_many :images, :through => :image_pages, :class_name => 'Refinery::Image', :order => 'position ASC'
         # accepts_nested_attributes_for MUST come before def images_attributes=
         # this is because images_attributes= overrides accepts_nested_attributes_for.
 
         accepts_nested_attributes_for :images, :allow_destroy => false
 
+        attr_accessor :image_pages_marked_for_deletion
+
+        after_save :delete_image_pages_as_necessary
         # need to do it this way because of the way accepts_nested_attributes_for
         # deletes an already defined images_attributes
         module_eval do
+          def delete_image_pages_as_necessary
+            if @image_pages_marked_for_deletion
+              @image_pages_marked_for_deletion.each do |image_page|
+                image_page.destroy
+              end
+            end
+          end
+
+
           def images_attributes=(data)
-            data = data.reject {|key, data| data.blank?}
-            ids_to_keep = data.map{|i, d| d['image_page_id']}.compact
+            ids_to_keep = data.map{|i, d| d['id']}.compact
 
             image_pages_to_delete = if ids_to_keep.empty?
-              self.image_pages
+              @image_pages_marked_for_deletion = self.image_pages.all
+              self.image_pages.delete_if {true}
             else
-              self.image_pages.where(
-                Refinery::ImagePage.arel_table[:id].not_in(ids_to_keep)
-              )
+              @image_pages_marked_for_deletion = self.image_pages.where(Refinery::ImagePage.arel_table[:image_id].not_in(ids_to_keep)).all
+              self.image_pages.delete_if { |ip| !ids_to_keep.include?(ip.image_id.to_s)}
             end
-
-            image_pages_to_delete.destroy_all
 
             data.each do |i, image_data|
               image_page_id, image_id, caption =
@@ -32,20 +41,17 @@ module Refinery
 
               next if image_id.blank?
 
-              image_page = if image_page_id.present?
-                self.image_pages.find(image_page_id)
-              else
-                self.image_pages.build(:image_id => image_id)
-              end
+              image_page = (self.image_pages.detect {|ip| ip.image_id.to_s == image_id.to_s } || self.image_pages.build(:image_id => image_id))
 
               image_page.position = i
               image_page.caption = caption if Refinery::PageImages.captions
-              image_page.save
             end
           end
         end
 
         include Refinery::PageImages::Extension::InstanceMethods
+
+        attr_accessible :images_attributes
       end
 
       module InstanceMethods
